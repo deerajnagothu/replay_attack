37548beda93586d34bd955a0dbcc9750e107f022
jschwel1 <jschwel1@binghamton.edu>
2018-09-10 11:05:49 -0400

    Added resize factor so image will be smaller and lag can be decreased. Added more comments

--- replay_attack.py
+++ replay_attack.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3.6
 import numpy as np
 import cv2
-from copy import deepcopy
-import random
 
 # Set up to default camera
 cap = cv2.VideoCapture(0)
@@ -16,10 +14,15 @@ attack_idx = 0      # Frame index for attack loop
 FRAME_TITLE = 'Live Camera'
 REPLAY_TITLE = 'Attacked Camera'
 
+# Change the size of the frames. This can significantly
+# decrease lag on less powerful machines
+RESIZE_FACTOR = 0.5
+
 while True:
     # Get the frame (and a T/F value)
     ret, frame = cap.read()
-
+    size=np.shape(frame)
+    frame = cv2.resize(frame,None,fx=RESIZE_FACTOR,fy=RESIZE_FACTOR)
     # If recording, append the current frame to the list
     if (recording):
         replay_attack.append(frame)
@@ -36,14 +39,16 @@ while True:
         else:
             # Display the frames from the list of recordings
             attack_frame = replay_attack[attack_idx]
+            # Change each pixel by [-1,1]
             dup_frame = np.add(attack_frame,
                             (np.random.rand(len(attack_frame), len(attack_frame[0]), 3)*4-2))
+            # Clip the pixel values at 0 and 255 so they can all be displayed
             np.clip(dup_frame, 0, 255, out=dup_frame)
+            # Convert the array back to values of uint8
             dup_frame = dup_frame.astype('uint8')
-
+            # Show the frame
             cv2.imshow(REPLAY_TITLE, dup_frame)
 
-            
 #            cv2.imshow(REPLAY_TITLE, replay_attack[attack_idx])
             attack_idx = (attack_idx+1)%len(replay_attack)
     else:
